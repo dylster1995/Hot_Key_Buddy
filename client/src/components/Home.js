8752eba48104d3431b380f6170162a01f5188400
naps&snacks <j.antoniogarcia1130@gmail.com>
2022-12-15 02:20:52 -0600

    styles for home.css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-
+import '../styles/Home.css'
 const Home = () => {
   
   return (
     <>
-      <div>
-        <h1>Hot Key Buddy</h1>
+    <body>
+      <div className="title">
+        <header>Hot Key Buddy</header>
       </div>
-      <div>
+      <div className="text">
         <h2>
-          Type the hot keys you'd like to save and and they'll display on the
+          Type in the hot keys you'd like to save and they'll display in the
           box bellow!
         </h2>
       </div>
-      <div>
-        
+      <div className="screen">
+        <p>Display</p>
       </div>
+      <div className="keyboard">
+        <p>Keyboard</p>
+      </div>
+      </body>
     </>
   );
 };
