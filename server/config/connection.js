84d93e85e2e1d05b7f91d4db1f4f741f97209211
Dylan Yamashiro <dylanyamashiro@gmail.com>
2022-12-19 04:20:51 -0800

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
-
-const localDB = 'mongodb://localhost:27017/hot_key_buddy_db';
-//process.env.MONGODB_URI
-
-// mongoose.connect('mongodb+srv://hot_key_buddy_user:8SCUaS2M9V0SjoD3@cluster0.zcgxe.mongodb.net/?retryWrites=true&w=majority', {
-  mongoose.connect('mongodb+srv://hot_key_buddy_user:8SCUaS2M9V0SjoD3@cluster0.zcgxe.mongodb.net/hot_key_buddy_db?authSource=admin&replicaSet=atlas-v3ytle-shard-0&readPreference=primary&ssl=true', {
+  mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
