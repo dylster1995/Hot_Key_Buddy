60947817be4cd049bbce40a56e59054958299b4f
Dylan Yamashiro <dylanyamashiro@gmail.com>
2022-12-19 05:36:32 -0800

    db heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
   mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
